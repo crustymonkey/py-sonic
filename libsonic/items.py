c7f9cb5849b51cb02789cc091614d21e34f07113
Jay Deiman <jay@splitstreams.com>
2011-05-01 23:04:04 -0500

    Started the work on the lazy attribute selection

--- libsonic/items.py
+++ libsonic/items.py
@@ -3,31 +3,61 @@ This defines all the various class items, such as Album, Artist, Song, etc.
 """
 
 from datetime import datetime
+import weakref
 
 class BaseSonicItem(object):
-    version = ''
+    def __init__(self):
+        self._postInit()
 
-class Response(BaseSonicItem):
-    pass
+    def _postInit(self):
+        pass
 
-class MusicFolder(BaseSonicItem):
-    mid = ''
-    name = ''
+class Response(Response):
+    def __init__(self , respDict , weakSonObj , weakParent=None):
+        super(Response , self).__init__()
+        self._setAttrItems(respDict)
+        self._weakParent = weakParent
+        self._weakSonObj = weakSonObj
+        self.fh = None
 
-class License(BaseSonicItem):
+    def __getattr__(self , name):
+        # Do the weak reference calls
+        if name == 'conn':
+            return self._weakSonObj()
+        if name == 'parent' and self._weakParent is not None:
+            return self._weakParent()
+        raise AttributeError(name)
+
+    def _setAttrItems(self , d):
+        for k , v in d.iteritems():
+            setattr(self , k , v)
+    
+class MusicFolder(Response):
+    def __getattr__(self , name):
+        try:
+            super(MusicFolder , self).__getattr__(name)
+        except AttributeError:
+            if name == 'artists':
+                self.artists = self._getArtists()
+                return self.artists
+
+    def _getArtists(self):
+        pass
+
+class License(Response):
     date = datetime.now()
     email = ''
     key = ''
     valid = False
 
-class Playlist(BaseSonicItem):
+class Playlist(Response):
     pid = ''
     name = ''
 
-class CoverArt(BaseSonicItem):
+class CoverArt(Response):
     cid = ''
 
-class User(BaseSonicItem):
+class User(Response):
     username = ''
     adminRole = False
     commentRole = False
@@ -40,21 +70,21 @@ class User(BaseSonicItem):
     uploadRole = True
     ldapAuthenticated = False
 
-class ChatMessage(BaseSonicItem):
+class ChatMessage(Response):
     message = ''
     date = datetime.now()
     username = ''
 
-class Lyric(BaseSonicItem):
+class Lyric(Response):
     artist = ''
     content = ''
     title = ''
 
-class Artist(BaseSonicItem):
+class Artist(Response):
     aid = ''
     name = ''
 
-class Album(BaseSonicItem):
+class Album(Response):
     aid = ''
     artist = ''
     coverArt = 'id'
@@ -62,7 +92,7 @@ class Album(BaseSonicItem):
     parent = 'parent id'
     title = ''
 
-class Song(BaseSonicItem):
+class Song(Response):
     sid = ''
     album = ''
     bitRate = 128
